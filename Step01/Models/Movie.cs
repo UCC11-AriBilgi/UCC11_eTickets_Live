52d583f0cfb70e4f2cfce3431f9e62fbb2e26a9b
ukaracivio <umit.karacivi@outlook.com>
2024-05-17 20:47:06 +0300

    Some changes

---
+++ Step01/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data.Enums;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public double? Price { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public MovieCategory? MovieCategory { get; set; } // Burayı besleyecek yer MovieCategory enum'ı
+
+    }
+}
---
+++ Step01/Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+
+        public string? ProfilePictureURL { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Bio { get; set; }
+    }
+}
