9eb017496e1fdfc6811b754547ac5bf36e69ae36
ukaracivio <umit.karacivi@outlook.com>
2024-05-20 15:25:14 +0300

    Some changes

--- Step01/Data/AppDbContext.cs
+++ Step01/Data/AppDbContext.cs
@@ -21,7 +21,9 @@ namespace eTickets.Data
             });
 
             // Actor_Movie <-->> Actor
-            object value = modelBuilder.Entity<Actor_Movie>().HasOne(m=> m.Actor).WithMany(acmo=> acmo.Actors_Movies);
+            object value = modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m=> m.Actor)
+                .WithMany(acmo=> acmo.Actors_Movies);
 
         }
 
