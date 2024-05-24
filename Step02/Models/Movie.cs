9a1b55c9592cb8e4b1017a1239468893db50ed3a
ukaracivio <umit.karacivi@outlook.com>
2024-05-24 20:52:36 +0300

    Some changes

--- Step02/Models/Movie.cs
+++ Step02/Models/Movie.cs
@@ -1,24 +1,33 @@
 ﻿using eTickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
     public class Movie
     {
+        [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Movie Name")]
         public string? Name { get; set; }
 
+        [Display(Name = "Description")]
         public string? Description { get; set; }
 
-        public double? Price { get; set; }
+        [Display(Name = "Price")]
+        public double Price { get; set; }
 
+        [Display(Name = "Poster")]
         public string? ImageURL { get; set; }
 
-        public DateTime? StartDate { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
 
-        public DateTime? EndDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
 
+        [Display(Name = "Category")]
         public MovieCategory? MovieCategory { get; set; } // Burayı besleyecek yer MovieCategory enum'ı
 
         // Relations
@@ -30,13 +39,13 @@ namespace eTickets.Models
         // One-to-Many
         public int CinemaId { get; set; }
         [ForeignKey("CinemaId")]
-        public Cinema Cinema { get; set; }
+        public Cinema? Cinema { get; set; }
 
         // Producer
         // One-to-Many
         public int ProducerId { get; set; }
         [ForeignKey("ProducerId")]
-        public Producer Producer { get; set;}
+        public Producer? Producer { get; set;}
 
     }
 }
