facc01e900ed189f9d2ac16c4d53972f412f7bf6
ilkeryolundagerek <ilkerturan@outlook.com>
2024-05-27 20:04:20 +0300

    Some changes

---
+++ Step03/Data/Interfaces/IActorsService.cs
@@ -0,0 +1,16 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Interfaces
+{
+    public interface IActorsService
+    {
+        // 20
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor actor);
+        void Delete(int id);
+    }
+}
---
+++ Step03/Data/Services/ActorsService.cs
@@ -0,0 +1,50 @@
+﻿using eTickets.Data.Interfaces;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActorsService(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        // 21
+        public IEnumerable<Actor> GetAll()
+        {
+            var actors = _context.Actors.ToList();
+
+            return actors;
+        }
+
+        public Actor GetById(int id)
+        {
+            var actor = _context.Actors.FirstOrDefault(a => a.Id == id);
+
+            return actor;
+        }
+
+       public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+    }
+}
