334e9506a901fcbaee86711d45dec5926a73c784
ilkeryolundagerek <ilkerturan@outlook.com>
2024-05-29 21:04:44 +0300

    Some changes

--- Step03/Controllers/ActorsController.cs
+++ Step03/Controllers/ActorsController.cs
@@ -78,5 +78,66 @@ namespace eTickets.Controllers
             return View(actorDetails);
         }
 
+        // 27
+        // Get : Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(actorDetails);
+        }
+
+        // 27
+        // Post : Actors/Edit/1
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            await _service.UpdateAsync(id, actor);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // 28
+        // Get : Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(actorDetails);
+
+        }
+
+        //28
+        // Post : Actors/Delete/1
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            await _service.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
